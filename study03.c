a3e872799eac87d4ac245c75bfc18909dc2de2a2
ajuyeon <ajuyeon@naver.com>
2022-09-12 21:06:27 +0900

    파일수정

--- study03.c
+++ study03.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int int main()
 {
-	int a = 30;
-	int b = 50;
-	printf("%d",a+b);
-	printf("%d",a-b);
+	int a = 300;
+	int b = 500;
+	printf("%d",a*b);
+	printf("%d",b%a);
 	return 0;
 }
