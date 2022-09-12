db0c4a0fb5edcaa97b3dac915ea498c919e7fd0c
ajuyeon <ajuyeon@naver.com>
2022-09-12 21:12:47 +0900

    수정

--- study03.c
+++ study03.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int int main()
 {
+<<<<<<< HEAD
 	int a = 300;
 	int b = 500;
 	printf("%d",a*b);
 	printf("%d",b%a);
+=======
+	int a = 3;
+	int b = 5;
+	printf("%d",a+b);
+>>>>>>> parent of 8874d8f (c언어 코드 수정)
 	return 0;
 }
